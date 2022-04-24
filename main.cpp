aeca6b76d43ff883f107d225ff4bbac3c1bd3efe
7ayd <86687700+7ayd@users.noreply.github.com>
2022-04-24 22:33:44 +0000

    fixed logic

--- main.cpp
+++ main.cpp
@@ -1,13 +1,88 @@
 #include <iostream>
 
+using namespace std; 
+
 class Node{
+public:
   int data;
   Node* next;
 };
 class Queue{
+private:  
+  Node* front; 
+  Node* rear; 
+  int count; 
 
+public:
+  void enqueue();
+  void dequeue();
+  bool isEmpty();
+  void print();
+  Queue(){
+    front = NULL;
+    rear = NULL;
+    count = 0; 
+  }
 };
 
 int main() {
-  std::cout << "Hello World!\n";
+  Queue test; 
+  cout << test.isEmpty() << endl; 
+  test.enqueue();
+  cout << test.isEmpty() << endl; 
+  test.dequeue();
+  test.enqueue();
+  test.enqueue();
+  test.enqueue();
+  test.enqueue();
+  test.dequeue();
+  cout << test.isEmpty() << endl;   
+}
+
+void Queue::enqueue(){
+  int _data;
+  Node* temp = new Node;
+  cout<<"Enter the data to enqueue: ";
+  cin>>_data;
+  temp->data = _data;
+  temp->next = NULL;
+  if(front == NULL){
+    front = temp;
+  }
+  else{
+  rear->next = temp;
+  }
+  rear = temp;
+  count++;
+}
+
+void Queue::dequeue(){
+  Node* temp = new Node;
+  if(front == NULL){
+  cout<<"\nQueue is Emtpty\n";  
+  }
+  else{
+  temp = front;
+  front = front->next;
+  cout<<"The data Dequeued is "<<temp->data << "\n";
+  delete temp;
+  }
+}
+
+bool Queue::isEmpty(){
+  if(front == NULL){
+    return true;
+  }
+  else{
+    return false; 
+  }
+}
+
+void Queue::print(){
+  Node* temp = new Node(); 
+  temp = front; 
+  while(temp->next != NULL){
+    cout << temp->data << " ";
+    temp->next = temp; 
+  }
 }
